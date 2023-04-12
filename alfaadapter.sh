0c1bdb46b61927a0d7399d8ca403ba1c480a35d3
ayman7-data <119970468+ayman7-data@users.noreply.github.com>
2023-04-12 22:03:00 +0300

    Add files via upload

---
+++ alfaadapter.sh
@@ -0,0 +1,11 @@
+﻿
+#!/bin/bash
+echo Remove RTL8187 Module
+rmmod rtl8187
+rfkill block all
+rfkill unblock all 
+echo Add RTL8187 Module 
+modprobe rtl8187 
+rfkill unblock all 
+echo Bring wlan0 up
+ifconfig wlane up
